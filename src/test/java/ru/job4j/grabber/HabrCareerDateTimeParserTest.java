515c2b87c9ddb53414b1bc00c2b4b7fac9c43bb2
Eidos <zaeho@mail.ru>
2023-06-25 14:03:30 +0300

    Правки к заданию 2.1. Преобразование даты [#289476]

--- src/test/java/ru/job4j/grabber/HabrCareerDateTimeParserTest.java
+++ src/test/java/ru/job4j/grabber/HabrCareerDateTimeParserTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class HabrCareerDateTimeParserTest {
 
     @Test
-    void ParseTest() {
+    void parseTest() {
         HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
         String date = "2023-06-22T14:27:15+03:00";
         String expected = "2023-06-22T14:27:15";
